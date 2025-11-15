f0c39154c074015560ff0c422494e818257c0055
Adesoye Adedokun <57882971+jeffrettad@users.noreply.github.com>
2025-11-15 00:13:37 -0800

    Update Layout.tsx

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -1,1 +1,14 @@
 
+import Header from "./Header";
+import Footer from "./Footer";
+import { ReactNode } from "react";
+
+const Layout = ({ children }: { children: ReactNode }) => (
+  <>
+    <Header />
+    <main className="min-h-screen">{children}</main>
+    <Footer />
+  </>
+);
+
+export default Layout;
